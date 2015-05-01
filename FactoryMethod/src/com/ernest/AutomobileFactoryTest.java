9d85b07e228b0952707cd6945d8e91c20f0fa8a9
James Byars <jbyars4ku@gmail.com>
2015-05-01 02:42:46 -0500

    Added basic tests

---
+++ FactoryMethod/src/com/ernest/AutomobileFactoryTest.java
@@ -0,0 +1,37 @@
+package com.ernest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by james on 5/1/15.
+ */
+public class AutomobileFactoryTest {
+
+    private AutomobileFactory factory;
+
+    @Before
+    public void setUp() {
+        factory = new AutomobileFactory();
+    }
+
+    @Test
+    public void canCreateFactoryTest() {
+        assertNotNull(factory);
+    }
+
+    @Test
+    public void testGetAutomobile() throws Exception {
+        Automobile sedan = null;
+        assertNull(sedan);
+
+        sedan = factory.getAutomobile("sedan");
+
+        assertNotNull(sedan);
+        assertTrue(sedan instanceof Automobile);
+        assertTrue(sedan instanceof SedanCar);
+
+    }
+}
